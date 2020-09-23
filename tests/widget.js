b6d22c07b66bdc0353834d75d549519af95c86c2
xiaohui lam <xiaohui.lam@icloud.com>
2020-09-23 15:16:33 +0800

    不传 unique_id 时，自动生成+持久化

--- tests/widget.js
+++ tests/widget.js
@@ -1,3 +1,27 @@
+var widget_origin = 'http://localhost:5000';
+var institution_id = 'rxpXD6uDD0EJqvbD';
+var userAgent = navigator.userAgent;
+var language = navigator.language;
+var url = location.href;
+var title = document.title;
+var name = '访客' + unique_id
+var referer = document.referrer;
+var unique_id;
+
+if (!unique_id) {
+  if (localStorage) {
+    if (!localStorage.kefuchat_unique_id) {
+      localStorage.kefuchat_unique_id = parseInt(Math.random()*99999);
+    }
+    unique_id = localStorage.kefuchat_unique_id
+  } else {
+    if (!document.cookie.match(/kefuchat_unique_id\=(^[;])/)) {
+      document.cookie = 'kefuchat_unique_id=' + parseInt(Math.random()*99999);
+    }
+    unique_id = document.cookie.match(/kefuchat_unique_id\=(^[;])/)[1]
+  }
+}
+
 var createDiv = function (__cls) {
   var div = document.createElement('div');
   div.className = __cls;
@@ -14,7 +38,7 @@ var installDom = function () {
   var iframe = document.createElement('iframe');
   kfdesc.innerText = '客服在线, 来咨询吧'
   iframe.className = 'kefuchat-iframe';
-  iframe.src = 'http://localhost:5000/?institution_id=rxpXD6uDD0EJqvbD&unique_id=3334&userAgent=Chrome&languages[]=zh-CN&url=https://www.ssls.com.cn&title=SSL&name=%E6%B5%8B%E8%AF%95%E8%AE%BF%E5%AE%A2&referer=https://www.baidu.com/';
+  iframe.src = widget_origin + '?institution_id=' + institution_id + '&unique_id=' + unique_id + '&userAgent=' + encodeURIComponent(userAgent) + '&languages[]=' + language + '&url=' + encodeURIComponent(url) + '&name=' + encodeURIComponent(name) + '&referer=' + encodeURIComponent(referer);
 
   opener.style.display = 'none'
   chat.style.display = 'none'
