a67fe5c4276f8bb6036175d743211c0e4a652fc6
xiaohui.lam <xiaohui.lam@icloud.com>
2020-10-11 16:07:21 +0800

    move method to root level

--- src/widget.js
+++ src/widget.js
@@ -1,7 +1,34 @@
+
 let kefu = {
   chat: {
     options: {},
-    init: function(conf) {
+    iframe: null,
+    initIframeDom: function () {
+      kefu.chat.iframe.contentDocument.writeln([
+        '<!DOCTYPE html>',
+        '<html lang="en">',
+        '<head>',
+        '<meta charset="utf-8" />',
+        '<meta name="viewport" content="width=device-width,initial-scale=1" />',
+        '<meta name="theme-color" content="#ffffff" />',
+        '<title>对话</title>',
+        '<link rel="stylesheet" href="' + kefu.chat.options.asset_origin + 'global.css" />',
+        '<link rel="stylesheet" href="' + kefu.chat.options.asset_origin + 'bundle.css" />',
+        '</head>',
+        '<body>',
+        '<p class="loading-bundle"> Loading bundle... You need JavaScript for this. <div class="centered">',
+        '<div class="loadingspinner">',
+        '</div>',
+        '</p>',
+        '<script>',
+        'window.parameters = ' + JSON.stringify(kefu.chat.options) + ';',
+        '</script>',
+        '<script async src="' + kefu.chat.options.asset_origin + 'bundle.js"></script>',
+        '</body>',
+        '</html>'
+      ].join(''));
+    },
+    init: function (conf) {
       let _kefuchat_init = (window)._kefuchat_init;
 
       if (_kefuchat_init && 'function' == typeof _kefuchat_init) {
@@ -15,25 +42,25 @@ let kefu = {
       }
 
       for (let j in conf) {
-        this.options[j] = conf[j];
+        kefu.chat.options[j] = conf[j];
       }
 
-      if (!this.options.unique_id) {
+      if (!kefu.chat.options.unique_id) {
         if (localStorage) {
           if (!localStorage.kefuchat_unique_id) {
             localStorage.kefuchat_unique_id = parseInt((Math.random() * 99999).toString());
           }
-          this.options.unique_id = localStorage.kefuchat_unique_id
+          kefu.chat.options.unique_id = localStorage.kefuchat_unique_id
         } else {
           if (!document.cookie.match(/kefuchat_unique_id\=(^[;])/)) {
             document.cookie = 'kefuchat_unique_id=' + parseInt((Math.random() * 99999).toString());
           }
-          this.options.unique_id = document.cookie.match(/kefuchat_unique_id\=(^[;])/)[1]
+          kefu.chat.options.unique_id = document.cookie.match(/kefuchat_unique_id\=(^[;])/)[1]
         }
       }
 
-      if (!this.options.name) {
-        this.options.name = '访客' + this.options.unique_id;
+      if (!kefu.chat.options.name) {
+        kefu.chat.options.name = '访客' + kefu.chat.options.unique_id;
       }
 
       let askNotificationPermission = () => {
@@ -48,7 +75,6 @@ let kefu = {
         });
       }
       let createDiv = (__cls) => {
-
         let div = document.createElement('div');
         div.className = __cls;
         return div;
@@ -67,43 +93,19 @@ let kefu = {
         let opener = createDiv('kefuchat-opener');
         container.appendChild(opener);
 
-        let kfdesc = createDiv('kefuchat-kfdesc');
         let logo = createDiv('kefuchat-logo');
         let badge = createDiv('kefuchat-badge');
-        let iframe = document.createElement('iframe');
-        chat.appendChild(iframe);
+        if (!kefu.chat.iframe) {
+          kefu.chat.iframe = document.createElement('iframe');
+          chat.appendChild(kefu.chat.iframe);
+          kefu.chat.iframe.className = 'kefuchat-iframe';
+          setTimeout(kefu.chat.initIframeDom, 1000);
+        }
+        let iframe = kefu.chat.iframe;
+        let kfdesc = createDiv('kefuchat-kfdesc');
         kfdesc.innerText = '客服在线, 来咨询吧'
-        iframe.className = 'kefuchat-iframe';
-        //iframe.src = this.options.chat_origin + '?institution_id=' + this.options.institution_id + '&unique_id=' + this.options.unique_id + '&userAgent=' + encodeURIComponent(this.options.userAgent) + '&languages[]=' + this.options.language + '&url=' + encodeURIComponent(this.options.url) + '&name=' + encodeURIComponent(this.options.name) + '&referer=' + encodeURIComponent(this.options.referer);
-        let initIframeDom = () => {
-          iframe.contentDocument.writeln(['<!DOCTYPE html>',
-            '<html lang="en">',
-            '<head>',
-            '<meta charset="utf-8" />',
-            '<meta name="viewport" content="width=device-width,initial-scale=1" />',
-            '<meta name="theme-color" content="#ffffff" />',
-            '<meta name="apple-mobile-web-app-capable" content="yes" />',
-            '<meta name="apple-mobile-web-app-status-bar-style" />',
-            '<meta name="theme-color" content="#ffffff" />',
-            '<title>对话</title>',
-            '<link rel="icon" type="image/png" href="' + this.options.asset_origin + 'favicon.png" />',
-            '<link rel="stylesheet" href="' + this.options.asset_origin + 'global.css" />',
-            '<link rel="stylesheet" href="' + this.options.asset_origin + 'bundle.css" />',
-            '<script>',
-            'window.parameters = ' + JSON.stringify(this.options) + ';',
-            '</script>',
-            '</head>',
-            '<body>',
-            '<p class="loading-bundle"> Loading bundle... You need JavaScript for this. <div class="centered">',
-            '<div class="loadingspinner">',
-            '</div>',
-            '</p>',
-            '<script async src="' + this.options.asset_origin + 'bundle.js"></script>',
-            '</body>',
-            '</html>'
-          ].join(''));
-        };
-        initIframeDom();
+        //iframe.src = kefu.chat.options.chat_origin + '?institution_id=' + kefu.chat.options.institution_id + '&unique_id=' + kefu.chat.options.unique_id + '&userAgent=' + encodeURIComponent(kefu.chat.options.userAgent) + '&languages[]=' + kefu.chat.options.language + '&url=' + encodeURIComponent(kefu.chat.options.url) + '&name=' + encodeURIComponent(kefu.chat.options.name) + '&referer=' + encodeURIComponent(kefu.chat.options.referer);
+
 
         opener.style.display = 'none'
         chat.style.display = 'none'
@@ -115,10 +117,10 @@ let kefu = {
 
       let installCss = () => {
         let css = document.createElement('link');
-        css.href = this.options.asset_origin + 'widget.css';
+        css.href = kefu.chat.options.asset_origin + 'widget.css';
         css.rel = 'stylesheet';
-        css.onload = function () {
-          show();
+        css.onload = () => {
+          document.querySelector('.kefuchat-opener').style.display = 'block';
           if ('function' == typeof (window).onloadKefuchat) {
             (window).onloadKefuchat()
           }
@@ -127,7 +129,7 @@ let kefu = {
       };
 
       (window).openKefuchat = () => {
-        askNotificationPermission().then(() => {});
+        askNotificationPermission().then(() => { });
         (document.querySelector('.kefuchat-opener')).style.display = 'none'; (document.querySelector('.kefuchat-chat')).style.display = 'block';
       };
 
@@ -176,14 +178,10 @@ let kefu = {
         }, false);
       };
 
-      let show = () => {
-        (document.querySelector('.kefuchat-opener')).style.display = 'block';
-      };
-
+      registerNotification();
       installDom();
       installCss();
       bindEvent();
-      registerNotification();
     },
 
     load: () => {
@@ -219,7 +217,19 @@ let kefu = {
       }
 
       kefu.chat.init(conf);
-    }
+    },
+
+    say: (msg) => {
+
+    },
+
+    open: (msg) => {
+      window.openKefuchat();
+    },
+
+    close: () => {
+      window.closeKefuchat();
+    },
   }
 };
 
